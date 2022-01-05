c94e4eb83d0192594e043e5c6e45eb62820499fb
Norska <norska99@gmail.com>
2022-01-05 15:08:55 +0200

    Build 1 Release

--- src/dev/norska/hexp/api/HarvestEXPAPI.java
+++ src/dev/norska/hexp/api/HarvestEXPAPI.java
@@ -7,9 +7,23 @@ import dev.norska.hexp.data.CropProfile;
 
 public class HarvestEXPAPI {
 	
+	/** 
+	* Check if a Block is a valid crop that the plugin utilizes.
+	* You can get the instance of HarvestEXP with HarvestEXP.getInstance();.
+	* @param HarvestEXP Instance, Block
+	* @return Boolean
+	*/
+	
 	public static Boolean isValidCrop(HarvestEXP instance, Block b) {
 		return instance.getCropProfileHandler().getCropProfile(b.getType()) != null;
 	}
+	
+	/** 
+	* Get the crop profile of a block to access it's settings (should be checked with #isValidCrop first).
+	* You can get the instance of HarvestEXP with HarvestEXP.getInstance();.
+	* @param HarvestEXP Instance, Block
+	* @return CropProfile
+	*/
 	
 	public static CropProfile getCropProfile(HarvestEXP instance, Block b) {
 		return instance.getCropProfileHandler().getCropProfile(b.getType());
